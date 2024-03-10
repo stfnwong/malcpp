cde26bd670bea057ef37ea919207b01046de1857
Stefan Wong <stfnwong@gmail.com>
2024-03-10 12:24:51 +0000

    An alternative test format

--- test/test_value.cpp
+++ test/test_value.cpp
@@ -12,11 +12,28 @@
 
 TEST_CASE("test_create_number_value", "value")
 {
-	Value v1(1.0f);
+	std::vector<Value> values = {
+		Value(1.0f), Value(1), Value(22222.2)
+	};
+	std::vector<ValueType> exp_types = {
+		ValueType::FLOAT, ValueType::INT, ValueType::FLOAT
+	};
 
-	// TODO: if ValueType becomes private then test against a string repr?
-	REQUIRE(v1.get_type() == ValueType::FLOAT);
+	std::vector<int> exp_values_as_int = {
+		1, 1, 22222
+	};
 
-	Value v2(2);
-	REQUIRE(v2.get_type() == ValueType::INT);
+	std::vector<double> exp_values_as_float = {
+		1.0, 1.0, 22222.2
+	};
+
+	// Ensure I wrote the test properly
+	REQUIRE(values.size() == exp_types.size());
+
+	for(unsigned i = 0; i < values.size(); ++i)
+	{
+		REQUIRE(values[i].get_type() == exp_types[i]);
+		REQUIRE(values[i].as_int() == exp_values_as_int[i]);
+		REQUIRE(values[i].as_float() == exp_values_as_float[i]);
+	}
 }
